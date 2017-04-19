62fc632c50af87c655acaeefc17b77e725aebc0d
Jakub Siwiński <kubsiw@gmail.com>
2017-04-19 23:31:57 +0200

    first ajax try ...

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 jQuery(document).ready(function(){
 
+    var show = $('.show');
+
+    show.on('click', function() {
+
+        var thisBtn = $(this);
+
+        $.ajax({
+            url: 'http://api.nbp.pl/api/exchangerates/rates/c/usd/2016-04-04/?format=json'
+            
+        }).done(function(response){
+            thisBtn.next().html(response).slideDown();
+        }).fail(function(error){ //... });
+
+        });
+    });
+
+    
+    
 }); // end of DOM
