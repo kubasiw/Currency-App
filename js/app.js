fadaf91513fe459892ef7a8d5a43105f9513725f
Jakub Siwiński <kubsiw@gmail.com>
2017-04-22 16:04:38 +0200

    crete code that get data from PKO api and inject currency data into table created also by function

--- js/app.js
+++ js/app.js
@@ -2,52 +2,40 @@ jQuery(document).ready(function(){
     
     
     
-
-    function currenciesAll() {
         
         
-        
-        $.ajax({
-            type: 'GET',
-            url: 'https://api.nbp.pl/api/exchangerates/tables/a/',
-            dataType: 'json'
-        }).done(function(data){
-            jQuery.each(data, function(i, item) {
-                console.log(data[0].rates);
-                var currencies = data[0].rates[1].mid;
-                console.log(currencies);
+        function getData() {
+            
+            $.ajax({
+                type: 'GET',
+                url: 'https://api.nbp.pl/api/exchangerates/tables/a/',
+                dataType: 'json'
                 
-            })
-        }).fail(function(error){ //... });
+            }).done(function(data){
+                
+                var dataArray = data[0].rates;
+                    console.log(dataArray);
+                    
+                var tr;
+                for (var i=0; i<dataArray.length; i++) {
+                    tr = jQuery('<tr/>');
+                    tr.append("<th>" + dataArray[i].index + "</th>");
+                    tr.append("<td>" + dataArray[i].currency + "</td>");
+                    tr.append("<td>" + dataArray[i].code + "</td>");
+                    tr.append("<td>" + dataArray[i].mid + "</td>");
+                    jQuery('tbody').append(tr);
+                };
+                
+            }).fail(function(error){ //... });
 
-        });
-        
-        var formControl1 = jQuery('.form-control1');
-        
-        var formControl2 = jQuery('.form-control2');
-        
-        jQuery('#btn').on('click', function(){
-            console.log(formControl1.val());
-            console.log(formControl1.val());
-        })
-    };
-    
-    currenciesAll();
-        
-    
+            });
 
-
-        
-
-    
-        
             
-            
+        };
+        getData();
+          
+    
+    
 
-    
-    
-   
-
-    
     
 }); // end of DOM
