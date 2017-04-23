a2a38d19ab1897ad409c401d716425f3470a4624
Jakub Siwiński <kubsiw@gmail.com>
2017-04-23 23:46:39 +0200

    first try of diffrence function

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ jQuery(document).ready(function(){
                 var dataArray1 = data[0].rates;
                 var dataArray2 = data[1].rates;
                 var currentDate = data[1].effectiveDate;
-//                    console.log(dataArray1);
+                    console.log(dataArray1);
 //                    console.log(dataArray2);
 //                    console.log(currentDate);
                 var title = jQuery('.title');
@@ -28,19 +28,29 @@ jQuery(document).ready(function(){
                 // and put it to new DOMelements (table)
                 for (var i=0; i<dataArray2.length; i++) {
                     
-                        tr = jQuery('<tr/>');
-                        tr.append("<th>" + (i+1) + "</th>");
-                        tr.append("<td>" + dataArray2[i].currency + "</td>");
-                        tr.append("<td>" + dataArray2[i].code + "</td>");
-                        tr.append("<td>" + dataArray2[i].mid.toFixed(3) + "</td>");
-                        
-                        jQuery('tbody').append(tr);
-                        tr.addClass('clickMe');
-    //                    jQuery('tr td:last-child').addClass('d-flex')
-    //                                              .addClass('justify-content-end');
-    //                    jQuery('tr th:last-child').addClass('d-flex')
-    //                                              .addClass('justify-content-end');
-                        
+                    tr = jQuery('<tr/>');
+                    tr.append("<th>" + (i+1) + "</th>");
+                    tr.append("<td>" + dataArray2[i].currency + "</td>");
+                    tr.append("<td>" + dataArray2[i].code + "</td>");
+                    tr.append("<td>" + dataArray2[i].mid.toFixed(3) + "</td>");
+
+                    jQuery('tbody').append(tr);
+                    tr.addClass('clickMe');
+                    jQuery('tr td:last-child').addClass('rateBox');
+                    
+                    jQuery('.rateBox').each(function(){
+                        for (var j=0; j<dataArray1.length; j++) {
+                            var $this = jQuery(this);
+                            if (dataArray2[i].mid > dataArray1[i].mid) {
+                                $this.addClass('growth');
+                            } else if (dataArray2[i].mid < dataArray1[i].mid) {
+                                $this.addClass('decrease');
+                            } else {
+                                $this.addClass('balance');
+                            };
+                        };
+                    });
+                    
                 };
             }).fail(function(error){ //... });
 
