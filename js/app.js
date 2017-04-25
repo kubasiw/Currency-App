9c1eb7dc322cd2b3950e033fe3ee6f5d0647199a
Jakub Siwiński <kubsiw@gmail.com>
2017-04-25 21:25:44 +0200

    add html for exchange app / start to creating exchange functions

--- js/app.js
+++ js/app.js
@@ -2,67 +2,81 @@ jQuery(document).ready(function(){
     
     
     
-        
-        // this function will get currency exchange data from NBP API
-        function getData() {
-            
-            $.ajax({
-                type: 'GET',
-                url: 'https://api.nbp.pl/api/exchangerates/tables/a/last/2',
-                dataType: 'json'
-                
-            }).done(function(data){
-                
-                // variable with all currencies / its array
-                var dataArray1 = data[0].rates;
-                var dataArray2 = data[1].rates;
-                var currentDate = data[1].effectiveDate;
-                    console.log(dataArray1);
+    function getData() {
+    // this function will get currency exchange data from NBP API
+
+        $.ajax({
+            type: 'GET',
+            url: 'https://api.nbp.pl/api/exchangerates/tables/a/last/2',
+            dataType: 'json'
+
+        }).done(function(data){
+
+            // variable with all currencies / its array
+            var dataArray1 = data[0].rates;
+            var dataArray2 = data[1].rates;
+            var currentDate = data[1].effectiveDate;
+                console.log(dataArray1);
 //                    console.log(dataArray2);
 //                    console.log(currentDate);
-                var title = jQuery('.title');
-                title.append("<span>" + "last update: " + currentDate + "</span>");
+            var title = jQuery('.title');
+            title.append("<span>" + "last update: " + currentDate + "</span>");
+
+            var tr;
+            // so we have to loop throught this array to get some interesting data
+            // and put it to new DOMelements (table)
+            for (var i=0; i<dataArray2.length; i++) {
+
+                tr = jQuery('<tr/>');
+                tr.append("<th>" + (i+1) + "</th>");
+                tr.append("<td>" + dataArray2[i].currency + "</td>");
+                tr.append("<td>" + dataArray2[i].code + "</td>");
+                tr.append("<td>" + dataArray2[i].mid.toFixed(3) + "</td>");
+
+                jQuery('tbody').append(tr);
+                tr.addClass('clickMe');
                 
-                var tr;
-                // so we have to loop throught this array to get some interesting data
-                // and put it to new DOMelements (table)
-                for (var i=0; i<dataArray2.length; i++) {
-                    
-                    tr = jQuery('<tr/>');
-                    tr.append("<th>" + (i+1) + "</th>");
-                    tr.append("<td>" + dataArray2[i].currency + "</td>");
-                    tr.append("<td>" + dataArray2[i].code + "</td>");
-                    tr.append("<td>" + dataArray2[i].mid.toFixed(3) + "</td>");
+                var tdCurr = jQuery('.clickMe td:nth-child(2)');
+                var tdCode = jQuery('.clickMe td:nth-child(3)');
+                var tdMid = jQuery('.clickMe td:nth-child(4)');
 
-                    jQuery('tbody').append(tr);
-                    tr.addClass('clickMe');
-                    jQuery('tr td:last-child').addClass('rateBox');
-                    
-                    jQuery('.rateBox').each(function(){
-                        for (var j=0; j<dataArray1.length; j++) {
-                            var $this = jQuery(this);
-                            if (dataArray2[i].mid > dataArray1[i].mid) {
-                                $this.addClass('growth');
-                            } else if (dataArray2[i].mid < dataArray1[i].mid) {
-                                $this.addClass('decrease');
-                            } else {
-                                $this.addClass('balance');
-                            };
-                        };
-                    });
-                    
+            tdCurr.addClass('curr');
+            tdCode.addClass('code');
+            tdMid.addClass('mid');
+                
+            };
+            
+            var select1 = jQuery('.select1');
+            var select2 = jQuery('.select2');
+            
+            for (var i=0; i<dataArray2.length; i++) {
+                
+                select1.append("<option>" + dataArray2[i].currency + " - " + dataArray2[i].code + "</option>");
+                select2.append("<option>" + dataArray2[i].currency + " - " + dataArray2[i].code + "</option>");
+                
+                var option1 = jQuery('.select1 option');
+                var option2 = jQuery('.select2 option');
+                
+                for (var j=0; j<option1.length; j++) {
+                    var $this = jQuery(this);
+                    if ((dataArray2[i].currency + " - " + dataArray2[i].code) === option1[j].innerText) {
+                        console.log(option1[i]);
+                    }
                 };
-            }).fail(function(error){ //... });
+            };
+            
+            
+        
 
-            });
-        };
+
+        }).fail(function(error){ //... });
+
+        });
+    };
+    getData();
+    
     
         
-    
-    
-    
-        getData();
-    
         
           
     
